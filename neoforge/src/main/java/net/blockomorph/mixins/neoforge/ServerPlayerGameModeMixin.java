166f9bf32a04055dbfc88a58fbbcea4092c21bec
151chit <151gamemode1chit@gmail.com>
2026-01-31 14:38:21 +0300

    fix multiblock breaking

--- neoforge/src/main/java/net/blockomorph/mixins/neoforge/ServerPlayerGameModeMixin.java
+++ neoforge/src/main/java/net/blockomorph/mixins/neoforge/ServerPlayerGameModeMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerPlayerGameMode;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -18,15 +17,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ServerPlayerGameModeMixin {
 	@Shadow @Final protected ServerPlayer player;
 
-	@Inject(method = "removeBlock", at = @At(value = "HEAD"), remap = false)
-	public void crackBlockStart(BlockPos pos, BlockState state, boolean canHarvest, CallbackInfoReturnable<Boolean> cir) {
-		InPlayerBlockPos.check(pos, (pl, realPos) -> {
-			pl.breakingModeStart(true);
-		}, null, false);
-	}
-
-	@Inject(method = "removeBlock", at = @At(value = "TAIL"), remap = false)
-	public void crackBlockEnd(BlockPos pos, BlockState state, boolean canHarvest, CallbackInfoReturnable<Boolean> cir) {
+	@Inject(method = "destroyBlock", at = @At(shift = At.Shift.AFTER, value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayerGameMode;removeBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Z)Z"), remap = false)
+	public void crackBlockEnd(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
 		InPlayerBlockPos.check(pos, (pl, realPos) -> {
 			boolean flag = pl.isBreaking();
 			pl.breakingModeStart(false);
