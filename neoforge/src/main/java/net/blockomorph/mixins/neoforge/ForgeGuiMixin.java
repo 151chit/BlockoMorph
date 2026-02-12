404527137f68af952ae949bcc2ea9d09bb4bb47b
151chit <151gamemode1chit@gmail.com>
2026-02-12 15:22:42 +0300

    fixes forge 1.20.1 gradle and work in launcher

--- neoforge/src/main/java/net/blockomorph/mixins/neoforge/ForgeGuiMixin.java
+++ neoforge/src/main/java/net/blockomorph/mixins/neoforge/ForgeGuiMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = ForgeGui.class, remap = false)
+@Mixin(value = ForgeGui.class)
 public class ForgeGuiMixin {
 
 	@Inject(method = "render", at = @At("TAIL"))
