cbd342278cdc586f00ec60364f93710cfcab9b8a
151chit <151gamemode1chit@gmail.com>
2026-02-12 16:25:34 +0300

    fix block particles on fabric

--- fabric/src/main/java/net/blockomorph/platformUtilsImpl/FabricClientUtils.java
+++ fabric/src/main/java/net/blockomorph/platformUtilsImpl/FabricClientUtils.java
@@ -139,6 +139,6 @@ public class FabricClientUtils implements ClientPlatformUtils {
 
 	@Override
 	public boolean isValidDestroyBlock(Level level, BlockPos keyPos, BlockState state, ParticleEngine engine) {
-		return true;//always true
+		return state.shouldSpawnTerrainParticles();
 	}
 }
