57d1f7b9a707b4f7240b66bfe209325a1a61f60a
151chit <151gamemode1chit@gmail.com>
2026-02-14 16:58:59 +0300

    fix fluid

--- common/src/main/java/net/blockomorph/mixins/main/PlayerMixin.java
+++ common/src/main/java/net/blockomorph/mixins/main/PlayerMixin.java
@@ -1,6 +1,8 @@
 package net.blockomorph.mixins.main;
 
 import com.google.gson.JsonPrimitive;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.JsonOps;
 import it.unimi.dsi.fastutil.longs.Long2LongMap;
@@ -42,6 +44,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockEventData;
 import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.AnvilBlock;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LiquidBlock;
@@ -679,6 +682,15 @@ public abstract class PlayerMixin extends LivingEntity implements PlayerAccessor
 		if (this.isActive()) {
 			cir.setReturnValue(HITBOX_HANDLER.getEyeHeight());
 		}
+	}
+
+	@WrapOperation(method = "travel", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"))
+	private BlockState modifyState(Level instance, BlockPos blockPos, Operation<BlockState> original) {
+		var entry = MorphUtils.getLiquidOnPos(this, this.position().add(0, 0.9, 0));
+		if (entry != null) {
+			return entry.getValue().getBlockState();
+		}
+		return original.call(instance, blockPos);
 	}
 
 	public PlayerMixin() {
