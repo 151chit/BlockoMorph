4dae2a3d8135294888cbfebe3ac286492c83ce7b
151chit <151gamemode1chit@gmail.com>
2026-03-08 17:38:26 +0300

    Postpone the implementation of BBE compatibility

--- common/src/main/java/net/blockomorph/utils/accessors/compat/BlockEntitySpecialRenderer.java
+++ common/src/main/java/net/blockomorph/utils/accessors/compat/BlockEntitySpecialRenderer.java
@@ -1,21 +1,14 @@
 package net.blockomorph.utils.accessors.compat;
 
-import net.blockomorph.mixins.compat.bbe.BBEconfigAccessor;
+import jdk.jfr.Description;
 import net.blockomorph.utils.platform.EarlyLoadingPlatformUtils;
 import oshi.annotation.concurrent.NotThreadSafe;
 
 public interface BlockEntitySpecialRenderer {
 	boolean BBE_LOADED = EarlyLoadingPlatformUtils.INSTANCE.isModLoaded("betterblockentities");
 
-	@NotThreadSafe
+	@NotThreadSafe @Description("TODO: Implement this function")
 	static void tryRenderWithoutOptimizations(Runnable action) {
-		if (!BBE_LOADED) {
-			action.run();
-			return;
-		}
-		boolean isOptimized = BBEconfigAccessor.isOptimized();
-		BBEconfigAccessor.setMasterOptimize(false);
 		action.run();
-		BBEconfigAccessor.setMasterOptimize(isOptimized);
 	}
 }
